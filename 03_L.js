882a6a692a988076a4b33cb41d693d0335da3224
tommios <tommios.net@gmail.com>
2020-03-28 17:29:21 +0200

    LSP - correct example!!!

--- 03_L.js
+++ 03_L.js
@@ -5,31 +5,39 @@
 //  Наследуемый класс должен дополнять, а не изменять базовый.
 
 class Person {
+
+}
+
+class Member extends Person {
     access() {
         console.log('У тебя есть доступ');
     }
 }
 
-class Frontend extends Person {
+class Guest extends Person {
+    isGuest = true;
+}
+
+class Frontend extends Member {
     canCreateFrontend() {
     }
 }
 
-class Backend extends Person {
+class Backend extends Member {
     canCreateBackend() {
     }
 }
 
-class PersonFromDifferentCompany extends Person {
+class PersonFromDifferentCompany extends Guest {
     access() {
         throw new Error('У тебя нет доступа! Иди к себе!');
     }
 }
 
-function openSecretDoor(person) {
-    person.access();
+function openSecretDoor(member) {
+    member.access();
 }
 
 openSecretDoor(new Frontend());
 openSecretDoor(new Backend());
-openSecretDoor(new PersonFromDifferentCompany());
+// openSecretDoor(new PersonFromDifferentCompany()); // There should be member!
