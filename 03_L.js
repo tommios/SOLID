372b91c2d422af246b49cdb2d7ad31802e2ca4cd
tommios <tommios.net@gmail.com>
2020-03-28 18:01:52 +0200

    LSP - correct example 2!

--- 03_L.js
+++ 03_L.js
@@ -41,3 +41,48 @@ function openSecretDoor(member) {
 openSecretDoor(new Frontend());
 openSecretDoor(new Backend());
 // openSecretDoor(new PersonFromDifferentCompany()); // There should be member!
+
+// =========================================
+// =============== Example 2 ===============
+// =========================================
+
+class Component {
+    isComponent = true
+}
+
+class ComponentWithTemplate extends Component {
+    render() {
+        return `<div>Component</div>`
+    }
+}
+
+class HigherOrderComponent extends Component {
+
+}
+
+class HeaderComponent extends ComponentWithTemplate {
+    onInit() {}
+}
+
+class FooterComponent extends ComponentWithTemplate {
+    afterInit() {}
+}
+
+class HOC extends HigherOrderComponent {
+    render() {
+        throw new Error('Render is impossible here')
+    }
+
+    wrapComponent(component) {
+        component.wrapped = true
+        return component
+    }
+}
+
+function renderComponent(component) {
+    console.log(component.render())
+}
+
+
+renderComponent(new HeaderComponent())
+renderComponent(new FooterComponent())
