b840030aa71e517c94657be1742dbacc9e1aa4e1
tommios <tommios.net@gmail.com>
2020-03-29 10:35:05 +0300

    ISP - correct example!

--- 04_I.js
+++ 04_I.js
@@ -3,59 +3,100 @@
 //  "много интерфейсов, специально предназначенных для клиентов, лучше,
 //     чем один интерфейс общего назначения.
 
+// class Animal {
+//     constructor(name) {
+//         this.name = name;
+//     }
+//
+//     walk() {
+//         console.log(`${this.name} умеет ходить`);
+//     }
+//
+//     swim() {
+//         console.log(`${this.name} умеет плавать`);
+//     }
+//
+//     fly() {
+//         console.log(`${this.name} умеет летать`);
+//     }
+// }
+//
+// class Dog extends Animal {
+//     fly() {
+//         return null;
+//     }
+// }
+//
+// class Eagle extends Animal {
+//     swim() {
+//         return null;
+//     }
+// }
+//
+// class Whale extends Animal {
+//     fly() {
+//         return null;
+//     }
+//
+//     walk() {
+//         return null;
+//     }
+// }
+
+// ==================================================
+// ==================================================
+
 class Animal {
     constructor(name) {
         this.name = name;
     }
+}
 
-    walk() {
-        console.log(`${this.name} умеет ходить`);
-    }
-
+const swimmer = {
     swim() {
         console.log(`${this.name} умеет плавать`);
     }
+}
 
+const flier = {
     fly() {
         console.log(`${this.name} умеет летать`);
     }
 }
 
-class Dog extends Animal {
-    fly() {
-        return null;
+const walker = {
+    walk() {
+        console.log(`${this.name} умеет ходить`);
     }
+}
+
+class Dog extends Animal {
 }
 
 class Eagle extends Animal {
-    swim() {
-        return null;
-    }
 }
 
 class Whale extends Animal {
-    fly(){
-        return null;
-    }
-
-    walk() {
-        return null;
-    }
 }
 
+Object.assign(Dog.prototype, swimmer, walker);
+Object.assign(Eagle.prototype, flier, walker);
+Object.assign(Whale.prototype, swimmer);
+
+console.log();
 const dog = new Dog('Рекс');
 dog.walk();
 dog.swim();
-dog.fly();
+//dog.fly();
 
 console.log();
 const eagle = new Eagle('Орел');
 eagle.fly();
-eagle.swim();
+//eagle.swim();
 eagle.walk();
 
 console.log();
 const whale = new Whale('Большой синий друг :)');
-whale.fly();
+//whale.fly();
 whale.swim();
-whale.walk();
+//whale.walk();
